6ea1ff52ae5378461ea5feec705d0500ca4c3dff
otammato <104728608+otammato@users.noreply.github.com>
2023-06-25 12:34:12 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,14 +1,47 @@
 var chai = require('chai');
 var io = require('socket.io-client');
 var app = require('../index');
-var client = io('http://localhost:3000');
 
 console.log('Server started...Waiting for client connection...');
 
 describe('Client Connection', function() {
+  // Function to check if a port is in use
+  function isPortInUse(port) {
+    return new Promise((resolve, reject) => {
+      const net = require('net');
+      const server = net.createServer()
+        .once('error', (err) => {
+          if (err.code === 'EADDRINUSE') {
+            resolve(true);
+          } else {
+            reject(err);
+          }
+        })
+        .once('listening', () => {
+          server.close(() => {
+            resolve(false);
+          });
+        })
+        .listen(port);
+    });
+  }
+
+  before(async function() {
+    const port = 3000;
+    const isPortUsed = await isPortInUse(port);
+    if (!isPortUsed) {
+      console.log('Server is not running on port 3000. Starting the server...');
+      // Start the server if it's not running already
+      // Your code to start the server here
+    } else {
+      console.log('Server is already running on port 3000.');
+    }
+  });
+
   it('should connect to the server', function(done) {
+    const client = io('http://localhost:3000');
     client.on('connect', function() {
-      done(); // Call done() to indicate the completion of the asynchronous operation
+      done();
     });
   });
 });
