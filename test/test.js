a1c9316f161372332193e85071e17840df8fec9b
otammato <104728608+otammato@users.noreply.github.com>
2023-06-25 12:50:32 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
-var chai = require('chai');
-var io = require('socket.io-client')
-var app = require('../index');
-var client = io('http://localhost:3001');
- 
-console.log('Server started...Waiting for client connection...');
-it('Client connected...', function(done) {
-  client.on('connect', function (data) {
-    done();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('App', function() {
+  it('should be running on port 3000', function(done) {
+    chai
+      .request('http://localhost:3000')
+      .get('/')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
