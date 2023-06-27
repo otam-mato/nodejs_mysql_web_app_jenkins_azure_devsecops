67955d84b92d38713fa66c3a69a5bd842a05e9db
otammato <104728608+otammato@users.noreply.github.com>
2023-06-27 20:12:54 +0100

    Update mysql_test.js

--- test/mysql_test.js
+++ test/mysql_test.js
@@ -1,15 +1,16 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const dbConfig = require("../../config/config.js");
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('MySQL Container', function() {
-  const mysqlHost = 'localhost'; // Update with the host where MySQL is running
-  const mysqlPort = 3306; // Update with the port on which MySQL is listening
-  const mysqlUser = 'root'; // Update with the MySQL user
-  const mysqlPassword = 'password'; // Update with the MySQL password
-  const mysqlDatabase = 'test'; // Update with the MySQL database name
+  const mysqlHost = dbConfig.APP_DB_HOST; // Update with the host where MySQL is running
+  const mysqlPort = dbConfig.APP_DB_PORT; // Update with the port on which MySQL is listening
+  const mysqlUser = dbConfig.APP_DB_USER; // Update with the MySQL user
+  const mysqlPassword = dbConfig.APP_DB_PASSWORD; // Update with the MySQL password
+  const mysqlDatabase = dbConfig.APP_DB_NAME; // Update with the MySQL database name
 
   it('should be able to query the database', function(done) {
     const query = 'SELECT 1 + 1 AS result';
@@ -24,3 +25,4 @@ describe('MySQL Container', function() {
       });
   });
 });
+
