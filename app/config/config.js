f0191df9823fc1e422d4bddae18ba9bba1194e61
otammato <104728608+otammato@users.noreply.github.com>
2023-06-27 20:32:23 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 // define default config, but allow overrides from ENV vars
 let config = {
   APP_DB_HOST: "localhost",
-  APP_DB_USER: "admin",
+  APP_DB_USER: "root",
   APP_DB_PORT: "3306" ,
   APP_DB_PASSWORD: "12345678",
   APP_DB_NAME: "COFFEE"
