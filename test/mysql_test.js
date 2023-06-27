2a006ec23447df28f7accdd3ec64e4bb362c554a
otammato <104728608+otammato@users.noreply.github.com>
2023-06-27 20:16:31 +0100

    Update mysql_test.js

--- test/mysql_test.js
+++ test/mysql_test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const dbConfig = require("../../config/config.js");
+const dbConfig = require("app/config/config.js");
 
 const expect = chai.expect;
 chai.use(chaiHttp);
