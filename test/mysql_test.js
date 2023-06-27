9c7dbd268bcbc94d3ac9bac4ff9e9e8a04169bbd
otammato <104728608+otammato@users.noreply.github.com>
2023-06-27 22:34:07 +0100

    Update mysql_test.js

--- test/mysql_test.js
+++ test/mysql_test.js
@@ -3,7 +3,6 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-const { describe, it } = require('mocha');
 
 describe('MySQL Container', function() {
   it('should be able to connect to the database', function(done) {
