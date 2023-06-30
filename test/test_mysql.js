7410cce4ec52f610bc98e1fbd666c2b4ca99168a
otammato <104728608+otammato@users.noreply.github.com>
2023-06-30 21:46:23 +0100

    Update test_mysql.js

--- test/test_mysql.js
+++ test/test_mysql.js
@@ -13,7 +13,9 @@ describe('App', function() {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        // Add additional assertions as needed to validate the response data
+        expect(res.body).to.be.an('array'); // Ensure the response is an array
+        expect(res.body.length).to.be.greaterThan(0); // Ensure the response array is not empty
+
         done();
       });
    });
