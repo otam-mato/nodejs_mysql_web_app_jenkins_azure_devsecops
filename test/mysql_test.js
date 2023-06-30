5d1b34a0feb20a46e21577edbb22bd4758bb97bb
otammato <104728608+otammato@users.noreply.github.com>
2023-06-30 15:18:14 +0100

    Update mysql_test.js

--- test/mysql_test.js
+++ test/mysql_test.js
@@ -1,5 +1,4 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require('chai');const chaiHttp = require('chai-http');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -15,12 +14,15 @@ describe('MySQL Container', function() {
       });
   });
 
-  it('should have the "COFFEE" database', function(done) {
-    chai.request('http://localhost:3000')
-      .get('/suppliers')
+  it('should return all entries in the database as JSON', function(done) {
+    chai
+      .request('http://localhost:3000')
+      .get('/entries')
       .end(function(err, res) {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        // Add additional assertions as needed to validate the response data
         done();
       });
   });
