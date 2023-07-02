f18fcfe85a51401ad4300e152aff3fca3f835122
otammato <104728608+otammato@users.noreply.github.com>
2023-07-02 12:06:34 +0100

    Update test_mysql.js

--- test/test_mysql.js
+++ test/test_mysql.js
@@ -13,14 +13,6 @@ describe('App', function() {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        expect(res.body).to.be.an('array'); // Ensure the response is an array
-        expect(res.body.length).to.be.greaterThan(0); // Ensure the response array is not empty
-        
-        const supplier = res.body[0]; // Assuming the response contains an array of supplier objects
-        expect(supplier).to.have.property('id');
-        expect(supplier).to.have.property('name');
-        expect(supplier.id).to.be.a('number');
-        expect(supplier.name).to.be.a('string');
         done();
       });
    });
@@ -31,6 +23,40 @@ describe('App', function() {
       .get('/entries')
       .end(function(err, res) {
         expect(res.body).to.be.an('array'); // Ensure the response is an array
+        done();
+      });
+   });
+
+   it('ensure the response array is not empty', function(done) {
+    chai
+      .request('http://localhost:3000')
+      .get('/entries')
+      .end(function(err, res) {
+        expect(res.body.length).to.be.greaterThan(0); // Ensure the response array is not empty
+        done();
+      });
+   });
+
+   it('ensure the response array is not empty', function(done) {
+    chai
+      .request('http://localhost:3000')
+      .get('/entries')
+      .end(function(err, res) {
+        expect(res.body.length).to.be.greaterThan(0); // Ensure the response array is not empty
+        done();
+      });
+   });
+
+   it('returned object contains the necessary properties: "id" (and it is the number), "name" (and it is the string)', function(done) {
+    chai
+      .request('http://localhost:3000')
+      .get('/entries')
+      .end(function(err, res) {
+        const supplier = res.body[0]; // Assuming the response contains an array of supplier objects
+        expect(supplier).to.have.property('id');
+        expect(supplier).to.have.property('name');
+        expect(supplier.id).to.be.a('number');
+        expect(supplier.name).to.be.a('string');
         done();
       });
    });
